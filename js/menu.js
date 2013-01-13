52e8cbd5c99067a0896c406bc8b627867df56cee
Matthias Pfeil <matze@macbook-pro.fritz.box>
2013-01-13 20:22:17 +0100

    Updated images, tab navigation, hover effects

--- js/menu.js
+++ js/menu.js
@@ -4,48 +4,61 @@ $('#locateMe').popover({placement:'bottom', trigger:'hover'})
 
 $('#locateMe').click("onclick",locateMe)
 
-$('#overview').click(function(){
-	$('#myModalLabel').text("Overview");
-	$('#myTab a:first').tab('show');
-	$('#myModal').modal("show");
-})
-
-$('#overview').hover(
+$(".img-rounded").hover(
 	function(){
-		this.setAttribute('src', 'img/info_white.png');
+		switch(this.getAttribute("id"))
+		{
+			case "overview":
+				this.setAttribute('src', 'img/info_hover.png');
+				break;
+			case "tempHum":
+				this.setAttribute('src', 'img/temphum_hover.png');
+				break; 	
+			case "no2":
+				this.setAttribute('src', 'img/no2_hover.png');
+				break;
+			case "co":
+				this.setAttribute('src', 'img/co_hover.png');
+				break;
+		}	
 	},
 	function(){
-		this.setAttribute('src', 'img/info.svg');
+		switch(this.getAttribute("id"))
+		{
+			case "overview":
+				this.setAttribute('src', 'img/info.svg');
+				break;
+			case "tempHum":
+				this.setAttribute('src', 'img/temphum.svg');
+				break; 	
+			case "no2":
+				this.setAttribute('src', 'img/no2.svg');
+				break;
+			case "co":
+				this.setAttribute('src', 'img/co.svg');
+				break;
+		}
 	}
 )
 
-$('#tempHum').hover(function(){
-
-})
-
-$('#no').hover(function(){
-
-})
-
-$('#co2').hover(function(){
-
-})
-
-
-$('#tempHum').click(function(){
-	$('#myModalLabel').text("Temperature / Humidity");
-	$('#myTab a:first').tab('show');
-	$('#myModal').modal("show");
-})
-
-$('#no').click(function(){
-	$('#myModalLabel').text("Nitrogen Dioxide");
-	$('#myTab a:first').tab('show');
-	$('#myModal').modal("show");
-})
-
-$('#co2').click(function(){
-	$('#myModalLabel').text("Carbon Monoxide");
+$(".img-rounded").click(function(){
+	var capture;
+	switch(this.getAttribute("id"))
+	{
+		case "overview":
+			capture = "Overview";
+			break;
+		case "tempHum":
+			capture = "Temperature / Humidity";
+			break;
+		case "no2":
+			capture = "Nitrogen Dioxide";
+			break;
+		case "co":
+			capture = "Carbon Monoxide";
+			break;
+	}
+	$('#myModalLabel').text(capture);
 	$('#myTab a:first').tab('show');
 	$('#myModal').modal("show");
 })
