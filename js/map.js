7a43eee3be58a41582663eeefc414900c38b67c3
Matthias Pfeil <matze@wl-pool1-pnt-13668.uni-muenster.de>
2013-03-08 17:23:07 +0100

    Added diagram
    canvasJS with random data

--- js/map.js
+++ js/map.js
@@ -68,3 +68,42 @@ function myCallbackFunction(results)
 	
 	clusters.fire('data:loaded');
 }
+
+window.onload = function () {
+    
+   
+    var chart = new CanvasJS.Chart("chartContainer",
+    {
+      zoomEnabled: true,
+      title:{
+        text: "Stress Test: 100,000 Data Points", 
+      },
+      axisX:{
+        labelAngle: 30,
+      },
+      
+      axisY :{
+        includeZero:false
+      },
+      
+      data: data,  // random generator below
+      
+    });
+
+    chart.render();
+  }
+  
+   var limit = 100000;    //increase number of dataPoints by increasing this
+   
+   var y = 0;
+   var data = []; var dataSeries = { type: "line" };
+   var dataPoints = [];
+   for (var i = 0; i < limit; i += 1) {
+    y += (Math.random() * 10 - 5);
+    dataPoints.push({
+      x: i - limit / 2,
+      y: y,                
+    });
+  }
+  dataSeries.dataPoints = dataPoints;
+  data.push(dataSeries);   
