4db54b0b124cefd60e7e73463de25eaeb7b36352
Matthias Pfeil <matthias.pfeil@wwu.de>
2013-03-12 17:46:25 +0100

    Lanuv Parser Update
    now inserts observations

--- Parser/LANUV/main.py
+++ Parser/LANUV/main.py
@@ -6,13 +6,11 @@ Created on 30.12.2012
 import urllib2
 import logging
 import re
-#import httplib
-#import psycopg2
-#import sys
+import json
 import time
 from bs4 import BeautifulSoup
 
-sos_url = "http://giv-geosoft2d.uni-muenster.de/istsos/qualityschu"
+sos_url = "http://giv-geosoft2d.uni-muenster.de/istsosold/qualityschu"
 getCapabilities = "?request=getCapabilities&sections=operationsmetadata&service=SOS&version=1.0.0"
 
 logger = logging.getLogger('LANUV')
@@ -22,7 +20,6 @@ hdlr.setFormatter(formatter)
 logger.addHandler(hdlr)
 logger.setLevel(logging.INFO)
 
-station = ""
 ozon = ""
 no = ""
 no2 = ""
@@ -35,19 +32,129 @@ staub = ""
 
 def getStationsOfSOS():
     stations = []
-    response = urllib2.urlopen(sos_url+getCapabilities).read()
-    soup = BeautifulSoup(response)
     
-    procedures = soup.find('ows:operationsmetadata').find('ows:parameter', attrs = {'name' : 'procedure'}).find('ows:allowedvalues').find_all('ows:value')
+    #response = urllib2.urlopen(sos_url+getCapabilities).read()
+    response = urllib2.urlopen('http://giv-geosoft2d.uni-muenster.de/istsos/wa/istsos/services/lanuv/procedures/operations/getlist?_dc=1363089212183').read()
+    #response = urllib2.urlopen('http://giv-geosoft2d.uni-muenster.de/istsosold/wa/istsos/services/qualityschu/procedures/operations/getlist?_dc=1363089212183').read()
+    decoded_data = json.loads(response)
     
-    for procedure in procedures:
-        station = re.findall('[A-Z][A-Z][A-Z][A-Z0-9]',procedure.get_text())
-        stations.append(station[0])
+    for data in decoded_data["data"]:
+        temp = []
+        temp.append(data["name"])
+        temp.append(data["assignedid"])
+        stations.append(temp) 
     
     return stations
 
-def insertObservation():
-    return "asdf"
+def insertObservation(station,values,local):
+    
+    insert_Observation = '<?xml version="1.0" encoding="UTF-8"?>\n\
+<sos:InsertObservation\n\
+   xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance"\n\
+   xsi:schemaLocation="http://schemas.opengis.net/sos/1.0.0/sosAll.xsd"\n\
+   xmlns:sos="http://www.opengis.net/sos/1.0"\n\
+   xmlns:xlink="http://www.w3.org/1999/xlink"\n\
+   xmlns:sa="http://www.opengis.net/sampling/1.0"\n\
+   xmlns:swe="http://www.opengis.net/swe/1.0.1"\n\
+   xmlns:gml="http://www.opengis.net/gml/3.2"\n\
+   xmlns:ogc="http://www.opengis.net/ogc"\n\
+   xmlns:om="http://www.opengis.net/om/1.0" service="SOS" version="1.0.0" >\n\
+   <sos:AssignedSensorId>'+station[1]+'</sos:AssignedSensorId>\n\
+   <om:Observation>\n\
+    <om:procedure xlink:href="urn:ogc:def:procedure:x-istsos:1.0:'+station[0]+'"/>\n\
+    <om:samplingTime>\n\
+      <gml:TimePeriod>\n\
+        <gml:beginPosition>'+time.strftime("%Y-%m-%dT%H:00:00", local)+'</gml:beginPosition>\n\
+        <gml:endPosition>'+time.strftime("%Y-%m-%dT%H:00:00", local)+'</gml:endPosition>\n\
+      </gml:TimePeriod>\n\
+    </om:samplingTime>\n\
+    <om:observedProperty xlink:href="">\n\
+      <swe:CompositPhenomenon dimension="9">\n\
+        <swe:component xlink:href="urn:ogc:def:parameter:x-istsos:1.0:time:iso8601"/>\n\
+        <swe:component xlink:href="urn:ogc:def:parameter:x-istsos:1.0:meteo:air:humidity" />\n\
+        <swe:component xlink:href="urn:ogc:def:parameter:x-istsos:1.0:meteo:air:no" />\n\
+        <swe:component xlink:href="urn:ogc:def:parameter:x-istsos:1.0:meteo:air:no2" />\n\
+        <swe:component xlink:href="urn:ogc:def:parameter:x-istsos:1.0:meteo:air:wv" />\n\
+        <swe:component xlink:href="urn:ogc:def:parameter:x-istsos:1.0:meteo:air:temperature" />\n\
+        <swe:component xlink:href="urn:ogc:def:parameter:x-istsos:1.0:meteo:air:so2" />\n\
+        <swe:component xlink:href="urn:ogc:def:parameter:x-istsos:1.0:meteo:air:pm10" />\n\
+        <swe:component xlink:href="urn:ogc:def:parameter:x-istsos:1.0:meteo:air:ozone" />\n\
+      </swe:CompositPhenomenon>\n\
+    </om:observedProperty>\n\
+    <om:featureOfInterest xlink:href="urn:ogc:def:feature:x-istsos:1.0:Point:'+station[0]+'"/>\n\
+      <om:result>\n\
+        <swe:DataArray>\n\
+          <swe:elementCount>\n\
+            <swe:Count>\n\
+              <swe:value>9</swe:value>\n\
+            </swe:Count>\n\
+          </swe:elementCount>\n\
+          <swe:elementType name="SimpleDataArray">\n\
+              <swe:DataRecord definition="http://mmiws.org/ont/x/timeSeries">\n\
+                <swe:field name="Time">\n\
+                  <swe:Time definition="urn:ogc:def:parameter:x-istsos:1.0:time:iso8601"/>\n\
+                </swe:field>\n\
+                <swe:field name="humidity">\n\
+                  <swe:Quantity definition="urn:ogc:def:parameter:x-istsos:1.0:meteo:air:humidity">\n\
+                    <swe:uom code="%"/>\n\
+                  </swe:Quantity>\n\
+                </swe:field>\n\
+                <swe:field name="nitrogen monoxide">\n\
+                  <swe:Quantity definition="urn:ogc:def:parameter:x-istsos:1.0:meteo:air:no">\n\
+                    <swe:uom code="mug"/>\n\
+                  </swe:Quantity>\n\
+                </swe:field>\n\
+                  <swe:field name="nitrogen dioxide">\n\
+                  <swe:Quantity definition="urn:ogc:def:parameter:x-istsos:1.0:meteo:air:no2">\n\
+                    <swe:uom code="mug"/>\n\
+                  </swe:Quantity>\n\
+                  </swe:field>\n\
+                  <swe:field name="wind velocity">\n\
+                  <swe:Quantity definition="urn:ogc:def:parameter:x-istsos:1.0:meteo:air:wv">\n\
+                    <swe:uom code="m/s"/>\n\
+                  </swe:Quantity>\n\
+                  </swe:field>\n\
+                  <swe:field name="temperature">\n\
+                  <swe:Quantity definition="urn:ogc:def:parameter:x-istsos:1.0:meteo:air:temperature">\n\
+                    <swe:uom code="deg"/>\n\
+                  </swe:Quantity>\n\
+                  </swe:field>\n\
+                  <swe:field name="sulfur dioxide">\n\
+                  <swe:Quantity definition="urn:ogc:def:parameter:x-istsos:1.0:meteo:air:so2">\n\
+                    <swe:uom code="mug"/>\n\
+                  </swe:Quantity>\n\
+                  </swe:field>\n\
+                  <swe:field name="pm10">\n\
+                  <swe:Quantity definition="urn:ogc:def:parameter:x-istsos:1.0:meteo:air:pm10">\n\
+                    <swe:uom code="mug"/>\n\
+                  </swe:Quantity>\n\
+                  </swe:field>\n\
+                  <swe:field name="ozone">\n\
+                  <swe:Quantity definition="urn:ogc:def:parameter:x-istsos:1.0:meteo:air:ozone">\n\
+                    <swe:uom code="mug"/>\n\
+                  </swe:Quantity>\n\
+                  </swe:field>\n\
+              </swe:DataRecord>\n\
+          </swe:elementType>\n\
+        <swe:encoding>\n\
+          <swe:TextBlock tokenSeparator="," blockSeparator="@" decimalSeparator="."/>\n\
+        </swe:encoding>\n\
+        <swe:values>'+values+'</swe:values>\n\
+      </swe:DataArray>\n\
+    </om:result>\n\
+  </om:Observation>\n\
+</sos:InsertObservation>'
+    
+    print insert_Observation
+    
+    headers = {"Content-type": "application/raw", "Accept": "text/plain"}
+
+    request = urllib2.Request("http://giv-geosoft2d.uni-muenster.de/istsos/lanuv",insert_Observation,headers)
+    
+    handler = urllib2.urlopen(request)
+    
+    print handler.read()
+    
 
 if __name__ == '__main__':
     logger.info("Parse process started")
@@ -55,155 +162,31 @@ if __name__ == '__main__':
     stations = getStationsOfSOS()
     
     for i in range(len(stations)):
-        response = urllib2.urlopen('http://www.lanuv.nrw.de/luft/temes/heut/'+stations[i]+'.htm#jetzt').read()
+        response = urllib2.urlopen('http://www.lanuv.nrw.de/luft/temes/heut/'+stations[i][0]+'.htm#jetzt').read()
         soup = BeautifulSoup(response)
     
         localtime = time.localtime(time.time())
     
         try:
             rows = soup.find('td', text = re.compile(str(localtime[3]) +":00" ), attrs = {'class' : 'mw_zeit'}).findPrevious('tr').findAll('td')
-        
-            ozon = rows[2].get_text()
-            no = rows[3].get_text()
-            no2 = rows[4].get_text()
-            ltem = rows[5].get_text()
-            wri = rows[6].get_text()
-            wges = rows[7].get_text()
-            rfeu = rows[8].get_text()
-            so2 = rows[9].get_text()
-            staub = rows[10].get_text()
             
-            logger.info("Successfully parsed values for Station "+str(stations[i])+" for "+str(localtime[3]) +":00 : "+ozon+","+no+","+no2+","+ltem+","+wri+","+wges+","+rfeu+","+so2+","+staub)
+            ozon = rows[2].get_text().lstrip()
+            no = rows[3].get_text().lstrip()
+            no2 = rows[4].get_text().lstrip()
+            ltem = rows[5].get_text().lstrip()
+            wri = rows[6].get_text().lstrip()
+            wges = rows[7].get_text().lstrip()
+            rfeu = rows[8].get_text().lstrip()
+            so2 = rows[9].get_text().lstrip()
+            staub = rows[10].get_text().lstrip()
+            logger.info("Successfully parsed values for Station "+str(stations[i][0])+" for "+str(localtime[3]) +":00 : "+rfeu+","+no+","+no2+","+wges+","+ltem+","+so2+","+staub+","+ozon)
+            
+            iso_time = time.strftime("%Y-%m-%dT%H:00:00", localtime)
+            
+            values = iso_time+","+ozon+","+no+","+no2+","+ltem+","+wri+","+wges+","+rfeu+","+so2+","+staub
+            
+            insertObservation(stations[i],values, localtime)
         except AttributeError:
             logger.error("No values for LANUV Station "+str(stations[i])+" at "+str(localtime[3])+":00"+" available!")
     
-    register_sensor = '<?xml version="1.0" encoding="UTF-8"?>\n\
-<sos:RegisterSensor service="SOS"\n\
-    xsi:schemaLocation="http://schemas.opengis.net/sos/1.0.0/sosAll.xsd"\n\
-    xmlns:gml="http://www.opengis.net/gml"\n\
-    xmlns:xlink="http://www.w3.org/1999/xlink"\n\
-    xmlns:swe="http://www.opengis.net/swe/1.0.1"\n\
-    xmlns:om="http://www.opengis.net/om/1.0"\n\
-    xmlns="http://www.opengis.net/sos/1.0"\n\
-    xmlns:sos="http://www.opengis.net/sos/1.0"\n\
-    xmlns:ows="http://www.opengis.net/ows/1.1"\n\
-    xmlns:ogc="http://www.opengis.net/ogc"\n\
-    xmlns:tml="http://www.opengis.net/tml"\n\
-    xmlns:sml="http://www.opengis.net/sensorML/1.0.1"\n\
-    xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance"\n\
-    version="1.0.0">\n\
-<SensorDescription>\n\
-    <sml:member>\n\
-      <sml:System gml:id="H_TREVANO">\n\
-        <gml:name>H_TREVANO</gml:name>\n\
-        <sml:identification/>\n\
-        <sml:classification>\n\
-          <sml:ClassifierList>\n\
-            <sml:classifier name="System Type">\n\
-              <sml:Term definition="urn:ogc:def:classifier:x-istsos:1.0:systemType">\n\
-                <sml:value>insitu-fixed-point</sml:value>\n\
-              </sml:Term>\n\
-            </sml:classifier>\n\
-            <sml:classifier name="Sensor Type">\n\
-              <sml:Term definition="urn:ogc:def:classifier:x-istsos:1.0:sensorType">\n\
-                <sml:value>humidity sensor</sml:value>\n\
-              </sml:Term>\n\
-            </sml:classifier>\n\
-          </sml:ClassifierList>\n\
-        </sml:classification>\n\
-        <sml:capabilities>\n\
-          <swe:DataRecord/>\n\
-        </sml:capabilities>\n\
-        <gml:location>\n\
-          <gml:Point gml:id="trevano" srsName="EPSG:4326">\n\
-            <gml:coordinates>8.961435,46.028235,350</gml:coordinates>\n\
-          </gml:Point>\n\
-        </gml:location>\n\
-        <sml:outputs>\n\
-          <sml:OutputList>\n\
-            <sml:output name="output data">\n\
-              <swe:DataRecord definition="urn:ogc:def:dataType:x-istsos:1.0:timeSeries">\n\
-                <swe:field name="Time">\n\
-                  <swe:Time definition="urn:ogc:def:parameter:x-istsos:1.0:time:iso8601" gml:id="1">\n\
-                    <swe:uom code="iso8601"/>\n\
-                  </swe:Time>\n\
-                </swe:field>\n\
-                <swe:field name="air humidity">\n\
-                  <swe:Quantity definition="urn:ogc:def:parameter:x-istsos:1.0:meteo:air:humidity" gml:id="2">\n\
-                    <swe:uom code="%"/>\n\
-                  </swe:Quantity>\n\
-                </swe:field>\n\
-              </swe:DataRecord>\n\
-            </sml:output>\n\
-          </sml:OutputList>\n\
-        </sml:outputs>\n\
-      </sml:System>\n\
-    </sml:member>\n\
-  </SensorDescription>\n\
-  <ObservationTemplate>\n\
-    <om:Observation>\n\
-      <om:procedure xlink:href="urn:ogc:object:procedure:x-istsos:1.0:H_TREVANO"/>\n\
-      <om:samplingTime>\n\
-        <gml:TimePeriod>\n\
-          <gml:TimeLength/>\n\
-        </gml:TimePeriod>\n\
-      </om:samplingTime>\n\
-      <om:observedProperty>\n\
-        <swe:CompositPhenomenon dimension="2">\n\
-          <swe:component xlink:href="urn:ogc:def:parameter:x-istsos:1.0:time:iso8601"/>\n\
-          <swe:component xlink:href="urn:ogc:def:parameter:x-istsos:1.0:meteo:air:humidity"/>\n\
-        </swe:CompositPhenomenon>\n\
-      </om:observedProperty>\n\
-      <om:featureOfInterest xlink:href="trevano">\n\
-        <gml:Point gml:id="trevano" srsName="EPSG:4326">\n\
-          <gml:coordinates>8.961435,46.028235,350</gml:coordinates>\n\
-        </gml:Point>\n\
-      </om:featureOfInterest>\n\
-      <om:result>\n\
-        <swe:DataArray>\n\
-          <swe:elementCount>\n\
-            <swe:value>2</swe:value>\n\
-          </swe:elementCount>\n\
-          <swe:elementType name="SimpleDataArray" xlink:href="urn:ogc:def:dataType:x-istsos:1.0:timeSeriesDataRecord">\n\
-            <swe:DataRecord definition="urn:ogc:def:dataType:x-istsos:1.0:timeSeries">\n\
-              <swe:field name="Time">\n\
-                <swe:Time definition="urn:ogc:def:parameter:x-istsos::time:iso8601" gml:id="1">\n\
-                  <swe:uom code="iso8601"/>\n\
-                </swe:Time>\n\
-              </swe:field>\n\
-              <swe:field name="air humidity">\n\
-                <swe:Quantity definition="urn:ogc:def:parameter:x-istsos:1.0:meteo:air:humidity" gml:id="2">\n\
-                  <swe:uom code="%"/>\n\
-                </swe:Quantity>\n\
-              </swe:field>\n\
-            </swe:DataRecord>\n\
-          </swe:elementType>\n\
-          <swe:encoding>\n\
-            <swe:TextBlock blockSeparator="@" decimalSeparator="." tokenSeparator=","/>\n\
-          </swe:encoding>\n\
-          <swe:values/>\n\
-        </swe:DataArray>\n\
-      </om:result>\n\
-    </om:Observation>\n\
-  </ObservationTemplate>\n\
-</sos:RegisterSensor>'
     
-    
-    
-    
-    
-    #headers = {"Content-type": "application/raw", "Accept": "text/plain"}
-
-    #request = urllib2.Request("http://giv-geosoft2d.uni-muenster.de/istsos/test",register_sensor,headers)
-    
-    #handler = urllib2.urlopen(request)
-    
-    #print handler.read()
-    
-    #response = urllib2.urlopen('http://www.lanuv.nrw.de/luft/temes/heut/VACW.htm#jetzt')
-    #stationen = response.read()
-
-    #print stationen
-
-    #for line in re.finditer('stationen\\.(.+)\\.(.+)\s+=\s+(.+)', stationen):
-       # print line.group(1) + " - " + line.group(2) + " - " + line.group(3)
