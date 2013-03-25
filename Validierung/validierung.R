3463b620f8be16dafff7704954017551601ba957
Nikolai Gorte <n.gorte@gmail.com>
2013-03-25 20:40:55 +0100

    Minor code fixes, fixed typos, added comments

--- Validierung/validierung.R
+++ Validierung/validierung.R
@@ -1,10 +1,12 @@
 library(RODBC)
 
+
 #################################################################################
 ###############################findOutliers FUNCTION#############################
 #################################################################################
 
-## identifies and flags outliers in the measurements using running median
+## identifies and flags outliers in the measurements using running median and
+## left-sided window
 ## INPUTS: table (res), window size (ws)
 ## OUTPUT: updated table (res)
 
@@ -38,6 +40,14 @@ findOutlier <- function(res, ws){
 
 
 
+
+
+#################################################################################
+########################Database access and validation###########################
+#################################################################################
+
+
+
 ## Connect to database
 ch<-odbcConnect("PostgreSQL35W")
 
@@ -62,9 +72,8 @@ ws <- 7
 ## Get non-validated measurements plus related foi's of each foi by joining following tables:
 ## procedures, event_time, measures
 for(i in 1:length(fois[[1]])){
-
-## Get the non-validated data
-rawData <- sqlQuery(ch, paste("SELECT 
+  ## Get the non-validated data
+  rawData <- sqlQuery(ch, paste("SELECT 
                                id_prc, id_foi_fk, id_msr, id_eti_fk, id_qi_fk, id_opr_fk, val_msr 
                                FROM 
                                procedures INNER JOIN event_time ON procedures.id_prc=event_time.id_prc_fk
@@ -76,13 +85,10 @@ rawData <- sqlQuery(ch, paste("SELECT
                                procedures.id_foi_fk= (",fois[i,1],")
                                ORDER BY 
                                measures.id_msr")) 
-
-
-
-## If it's not the very first dataset, we need some already validated data to validate the first
-## k=ws measurments of rawData1
-if(min(rawData[,c("id_msr")]) >ws){
-helperData <- sqlQuery(ch, paste("SELECT
+  ## If it's not the very first dataset, we need some already validated data to validate the first
+  ## k=ws measurments of rawData
+  if(min(rawData[,c("id_msr")]) >ws){
+  helperData <- sqlQuery(ch, paste("SELECT
                                id_prc, id_foi_fk, id_msr, id_eti_fk, id_qi_fk, id_opr_fk, val_msr
                                FROM 
                                (SELECT * FROM (procedures INNER JOIN event_time ON procedures.id_prc=event_time.id_prc_fk
@@ -92,33 +98,29 @@ helperData <- sqlQuery(ch, paste("SELECT
                                AND 
                                measures.id_msr < (",min(rawData[,c("id_msr")]),")
                                ORDER BY 
-                               measures.id_msr DESC LIMIT 100) as T 
+                               measures.id_msr DESC LIMIT (",length(obspr[[1]])*ws,")) as T 
                                ORDER BY
                                T.id_msr ASC"))
+  
+  
+  ## prepared data
+  editData <- rbind(helperData, rawData)} else {editData <- rawData}
+  
+  
+  
+  ## gets the measured values for each observed propertie and validates them
+  ## if the table is not empty
+  for(j in 1:length(obspr[[1]])){
+    res<-editData[editData$id_opr_fk==obspr[j,1],]
+    
+    if (nrow(res)==0) j=j+1 else res <-findOutlier(res,ws)
+    
+    ## Update table
+    sqlUpdate(ch,res,"measures")
+  }
 
-
-## prepared data
-editData <- rbind(helperData, rawData)} else {editData <- rawData}
-
-
-
-for(j in 1:4){
-res<-editData[editData$id_opr_fk==obspr[j,1],]
-
-if (nrow(res)==0) j=j+1 else res <-findOutlier(res,ws)
 }
 
-
-}
-
-
-
-
-
-
-
-## Update table
-sqlUpdate(ch,res,"measures")
   
 
 ## close conection
