87ea80ae32824cb66664d70092d9eb9d171f7c43
Matthias Pfeil <matthias.pfeil@wwu.de>
2013-03-27 17:50:01 +0100

    Fixed COSM Parser

--- Parser/COSM/fetchData.py
+++ Parser/COSM/fetchData.py
@@ -7,17 +7,43 @@ import urllib2
 import logging
 import re
 import json
+from datetime import datetime, timedelta
+
+xApiKey = "tT0xUa9Yy24qPaZXDgJi-lDmf3iSAKxvMEhJWDBleHpMWT0g"
 
 def getAQEFromSos():
-    request = urllib2.urlopen("http://giv-geosoft2d.uni-muenster.de/istsos/wa/istsos/services/cosmtest/procedures/operations/getlist").read()
+    aqe = []
+    request = urllib2.urlopen("http://giv-geosoft2d.uni-muenster.de/istsos/wa/istsos/services/cosm/procedures/operations/getlist").read()
     
     decoded_data = json.loads(request)
-    print decoded_data
+    #print decoded_data
     for data in decoded_data["data"]:
-        print data["name"]
+        aqe.append(data["name"])
+        
+    getDatapointsFromCOSM(aqe)
 
-def getDatapointsFromCOSM():
-    print "asdf"
-
+def getDatapointsFromCOSM(aqe):
+    
+    one_hour_from_now = datetime.now() - timedelta(minutes=15)
+    print one_hour_from_now
+    starttime = '{:%Y-%m-%dT%H:%M:00}'.format(one_hour_from_now)
+    print starttime
+    endtime = '{:%Y-%m-%dT%H:%M:00}'.format(datetime.now())
+    print endtime
+    
+    for i in range(len(aqe)):
+        try:
+            response = urllib2.urlopen("http://api.cosm.com/v2/feeds/"+aqe[i]+".json?start="+starttime+"&end="+endtime+"&timezone=+1&interval=60&interval_type=discrete&find_previous=true&key="+xApiKey).read()
+            history = json.loads(response)
+            datastreams = history["datastreams"]
+            print len(datastreams)
+            for j in range(len(datastreams)):
+                print datastreams[j]["tags"]
+        except KeyError as kE:
+            print "Error:"+str(kE)+" not found for "+str(datastreams[j])
+        #print datastream[0]["datapoints"]
+        #print datastream[0]["datapoints"][0]
+        #for datastream in decoded_datapoints["datastreams"]:
+        #    print datastream["datapoints"]
 if __name__ == '__main__':
     getAQEFromSos()
--- Parser/COSM/newandupdate.py
+++ Parser/COSM/newandupdate.py
@@ -11,11 +11,13 @@ import json
 xApiKey = "tT0xUa9Yy24qPaZXDgJi-lDmf3iSAKxvMEhJWDBleHpMWT0g"
 
 def getAQEFeeds():
-    #request = urllib2.urlopen("http://api.cosm.com/v2/feeds?key="+xApiKey+"&user=airqualityegg&status=live&per_page=150").read()
-    request = urllib2.urlopen("http://api.cosm.com/v2/feeds?key="+xApiKey+"&q=aqe&content=summary&status=live&per_page=150").read()
+    request = urllib2.urlopen("http://api.cosm.com/v2/feeds?key=tT0xUa9Yy24qPaZXDgJi-lDmf3iSAKxvMEhJWDBleHpMWT0g&q=aqe&content=summary&status=live&per_page=150").read()
+    #request = urllib2.urlopen("http://api.cosm.com/v2/feeds?key="+xApiKey+"&q=aqe&content=summary&status=live&per_page=150").read()
     
     decoded_data = json.loads(request)
+    
     for data in decoded_data["results"]:
+        #print data
         try:
             if str(data["id"]) == "77464":
                 break
@@ -23,12 +25,12 @@ def getAQEFeeds():
                 keywords = data["location"]["domain"]+","+data["location"]["exposure"]
                 lat = str(data["location"]["lat"])
                 lon = str(data["location"]["lon"])
-           
-                register_Sensor = '{"system_id":"'+str(data["id"])+'","system":"'+str(data["id"])+'","description":"","keywords":"'+keywords+'","identification":[],"classification":[{"name":"System Type","definition":"urn:ogc:def:classifier:x-istsos:1.0:systemType","value":"insitu-fixed-point"},{"name":"Sensor Type","definition":"urn:ogc:def:classifier:x-istsos:1.0:sensorType","value":"aqe"}],"characteristics":"","contacts":[],"documentation":[],"capabilities":[],"location":{"type":"Feature","geometry":{"type":"Point","coordinates":["'+lon+'","'+lat+'"," "]},"crs":{"type":"name","properties":{"name":"4326"}},"properties":{"name":"'+str(data["id"])+'"}},"interfaces":"","inputs":[],"outputs":[{"name":"Time","definition":"urn:ogc:def:parameter:x-istsos:1.0:time:iso8601","uom":"iso8601","description":"","constraint":{"role":null,"interval":null}},{"name":"humidity","definition":"urn:ogc:def:parameter:x-istsos:1.0:meteo:air:humidity","uom":"%","description":"","constraint":{"role":"","interval":[null,null]}}],"history":[]}'
-                #print register_Sensor
+                
+                register_sensor = '{"system_id":"'+str(data["id"])+'","system":"'+str(data["id"])+'","description":"","keywords":"'+keywords+'","identification":[],"classification":[{"name":"System Type","definition":"urn:ogc:def:classifier:x-istsos:1.0:systemType","value":"insitu-fixed-point"},{"name":"Sensor Type","definition":"urn:ogc:def:classifier:x-istsos:1.0:sensorType","value":"aqe"}],"characteristics":"","contacts":[],"documentation":[],"capabilities":[],"location":{"type":"Feature","geometry":{"type":"Point","coordinates":["'+lon+'","'+lat+'","0"]},"crs":{"type":"name","properties":{"name":"4326"}},"properties":{"name":"'+str(data["id"])+'"}},"interfaces":"","inputs":[],"outputs":[{"name":"Time","definition":"urn:ogc:def:parameter:x-istsos:1.0:time:iso8601","uom":"iso8601","description":"","constraint":{"role":null,"interval":null}},{"name":"co","definition":"urn:ogc:def:parameter:x-istsos:1.0:meteo:air:co","uom":"ppb","description":"","constraint":{"role":null,"interval":["",""]}},{"name":"humidity","definition":"urn:ogc:def:parameter:x-istsos:1.0:meteo:air:humidity","uom":"%","description":"","constraint":{"role":null,"interval":["",""]}},{"name":"no2","definition":"urn:ogc:def:parameter:x-istsos:1.0:meteo:air:no2","uom":"ppb","description":"","constraint":{"role":null,"interval":["",""]}},{"name":"temperature","definition":"urn:ogc:def:parameter:x-istsos:1.0:meteo:air:temperature","uom":"deg","description":"","constraint":{"role":null,"interval":["",""]}}],"history":[]}'
+                print register_sensor
                 
                 headers = {"Content-type": "application/raw", "Accept": "text/plain"}
-                request = urllib2.Request("http://giv-geosoft2d.uni-muenster.de/istsos/wa/istsos/services/cosmtest/procedures",register_Sensor,headers)
+                request = urllib2.Request("http://giv-geosoft2d.uni-muenster.de/istsos/wa/istsos/services/cosm/procedures",register_sensor,headers)
                 handler = urllib2.urlopen(request)
                 response = handler.read()
                 response_data = json.loads(response)
