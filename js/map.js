2e9ec12c93bb18e47d44becf9180535e43c51fc8
Matthias Pfeil <matthias.pfeil@wwu.de>
2013-04-02 23:12:39 +0200

    Added timezone support

--- js/map.js
+++ js/map.js
@@ -132,10 +132,38 @@ function addAQE(results)
      });
 }
 
+function getTz(offset)
+{
+	offset = offset / -60;
+	if (offset>=0 && offset <10)
+	{
+		strTz = "+0"+String(offset);
+	}
+	else if (offset>=0 && offset >= 10)
+	{
+		strTz = String(offset);	
+	}
+	else if (offset<=0 && offset <10)
+	{
+		strTz = "-0"+String(offset);	
+	}
+	else if (offset<=0 && offset >10)
+	{
+		strTz = String(offset);	
+	}
+
+	return strTz;
+}
+
 function updateTable(start,end)
 {
-	eventtime_start = start.toString('yyyy-MM-ddT00:00:00+01:00');
-	eventtime_end = end.toString('yyyy-MM-ddT23:00:00+01:00');
+	eventtime_start = start.toString('yyyy-MM-ddT00:00:00');
+	eventtime_start = eventtime_start + getTz(start.getTimezoneOffset());
+	eventtime_end = end.toString('yyyy-MM-ddT23:00:00');
+	eventtime_end = eventtime_end + getTz(end.getTimezoneOffset());
+	// console.log(eventtime_start);
+	// console.log(eventtime_end);
+	
 	var td = new Array();
 	jQuery.ajax({
         url: 'http://giv-geosoft2d.uni-muenster.de/istsos/wa/istsos/services/lanuv/operations/getobservation/offerings/temporary/procedures/'+checkedStation+'/observedproperties/urn:ogc:def:parameter:x-istsos:1.0:neteo:air:wv,urn:ogc:def:parameter:x-istsos:1.0:meteo:air:temperature,urn:ogc:def:parameter:x-istsos:1.0:meteo:air:so2,urn:ogc:def:parameter:x-istsos:1.0:meteo:air:pm10,urn:ogc:def:parameter:x-istsos:1.0:meteo:air:ozone,urn:ogc:def:parameter:x-istsos:1.0:meteo:air:no,urn:ogc:def:parameter:x-istsos:1.0:meteo:air:no2,urn:ogc:def:parameter:x-istsos:1.0:meteo:air:humidity/eventtime/'+eventtime_start+'/'+eventtime_end+'?_dc=1363547035889',
@@ -143,32 +171,44 @@ function updateTable(start,end)
         dataType: "json",
         success:function(data3){
         	data2 = data3;
+        	// console.log(data2);
         	for(var i = 0; i < data2.data[0].result.DataArray.values.length; i++)
         	{
         		newtablerow = "";
         		td = data2.data[0].result.DataArray.values[i];
-	        	date = new Date(td[0]).toString('d.MM.yyyy HH:mm:ss');
-	        	console.log(date.toString('d.MM.yyyy '));
+	        	// date = new Date(td[0]);
+	        	// printdate = date.toString('dd.MM.yyyy');
+	        	// // if (date.getTime > )
+	        	// console.log(printdate);
+	        	// hours = date.getHours()-1;
+	        	// printdate = printdate +" "+hours;
+	        	// printdate = printdate +":00:00";
+	        	// console.log(printdate);
+	        	// console.log(date.getHours()-1);
+	        	// console.log(String(date.getHours()-1));
+	        	date = new Date(td[0]).toString('dd.MM.yyyy HH:mm:ss');
+	        	// console.log(date);
+	        	// console.log(date.toString('dd.MM.yyyy '));
 	        	for(var j = 2; j < td.length; j = j + 2)
 	        	{
 		        	if(td[j]=="100")
 		        	{
-			        	console.log("raw");
+			        	// console.log("raw");
 			        	newtablerow = newtablerow + '<td class="tdwarning">'+td[j-1]+'</td>';
 		        	}
 		        	else if(td[j]=="101")
 		        	{
-			        	console.log("validated and outlier");
+			        	// console.log("validated and outlier");
 			        	newtablerow = newtablerow + '<td class="tderror">'+td[j-1]+'</td>';
 		        	}
 		        	else if(td[j]=="102")
 		        	{
-			        	console.log("validated and not outlier");
+			        	// console.log("validated and not outlier");
 			        	newtablerow = newtablerow + '<td class="tdsuccess">'+td[j-1]+'</td>';
 		        	}
 		        	else
 		        	{
-			        	console.log("NaN");
+			        	// console.log("NaN");
 			        	newtablerow = newtablerow + '<td>-</td>';
 		        	}
 	        	}
